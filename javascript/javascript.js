d6c3a79e97fa9537934a03ebb569c66e56aee91e
Edithnyiransengimana <nedithe1@gmail.com>
2023-04-24 11:32:52 -0700

    introducing javascript

---
+++ javascript/javascript.js
@@ -0,0 +1,33 @@
+//string concantinating
+//variable declaration
+//let,const //data
+//  (condition) {
+// }
+let firstname="edith";
+let secondname="nyiransengimana";
+// if(firstname==="edith"){
+// console.log("corect!!!!")}
+// else{
+//     console.log("incorect.....")
+// }
+//if (firstname==="edith"&& secondname==="nyiransengimana"){
+   // console.log("correct");}
+
+//else{
+  //  console.log("incorect")}
+
+if("a===2"||a==="3"){
+    console.log("yes");
+}
+else{
+    console.log("no")
+}
+
+
+
+
+//console.log("firstname" +" " +"secondname");
+//console.log("welcome to my page from javascript file")
+//console.log("2+2")
+//console.log("4")
+//console.log("edith")
