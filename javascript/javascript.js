bc99bc2e921a904c5150e4630569d7acd91e88ae
Edithnyiransengimana <nedithe1@gmail.com>
2023-04-25 13:07:35 -0700

    home page

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -16,14 +16,43 @@ let secondname="nyiransengimana";
 //else{
   //  console.log("incorect")}
 
-if("a===2"||a==="3"){
-    console.log("yes");
-}
-else{
-    console.log("no")
-}
+//if("a===2"||a==="3"){
+ //   console.log("yes");}
 
+//else{
+ //   console.log("no")}
 
+//let someone="18 years";
+//if(someone==="18 years"){
+ // console.log("allowed to drink beer")}
+
+//else{
+  //console.log("not allowed to drink beer")}
+  let n=1;
+  let n=2;
+  let n=3;
+  let n=4;
+
+  let students=["ndizihiwe","kanyana","nyiransengimana","munezero","ishimwe"];
+  let studentsAge=[1,2,3,4,5,6]
+  let a=2;
+  let b=0;
+  let sum=studentsAge[a]+studentsAge[b]
+  if(a<studentsAge.length && b<studentsAge.length ){
+    console.log("go on");
+    console.log("student is:",students[sum]);
+  }
+   else{
+   console.log("nooo")
+   
+  
+
+ // let sum = studentsAge[a] + studentsAge[b];
+ // if(sum < students.length){
+   console.log("student is:",students[sum]);  }
+ // else{
+   // console.log("authentical failed") }
+ 
 
 
 //console.log("firstname" +" " +"secondname");
